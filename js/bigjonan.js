7b6df9a14fa64deefd9944997c37f2b78f9c4199
Gustavo Ralek <gpenaloza@despegar.com>
2015-06-30 16:59:33 -0300

    agregados varios

--- js/bigjonan.js
+++ js/bigjonan.js
@@ -1,11 +1,37 @@
 	/*	Parallax
 	/* ---------------------------------------------------------------------- */
-    $('.parallax-banner').parallax({imageSrc: 'img/parallax/back.png'});
+function back(){
 
-    jQuery(window).trigger('resize').trigger('scroll');
+        if($('.parallax-banner').hasClass('parallax-back1'))
+        {
+            $('.parallax-banner').addClass('parallax-back2').removeClass('parallax-back1');
+        }
+        else
+        {
+            $('.parallax-banner').addClass('parallax-back1').removeClass('parallax-back2');
+        }
+
+}
 
 
-	
+
+
+    window.setInterval(function(){
+        /// call your function here
+        back();
+    }, 10000);
+
+
+    /*$( ".parallax-banner" ).click(function() {
+        $( "#book" ).animate({
+            opacity: 0.25,
+            left: "+=50",
+            height: "toggle"
+        }, 5000, function() {
+            // Animation complete.
+        });
+    });*/
+
     /*	Bullets Derecha
      /* ---------------------------------------------------------------------- */
 
